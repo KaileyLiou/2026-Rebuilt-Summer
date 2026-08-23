beadb83b0c8028065755c3a60bc1a0114eceb298
Kailey Liou <kaileyliou09@gmail.com>
2026-08-22 23:17:02 -0400

    added shooter ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,6 +3,9 @@ package frc.robot;
 public class Ports {
     public class ShooterPorts {
         public static final int INDEXER_MOTOR = 6;
+        public static final int SHOOTER_MOTOR = 14;
+        public static final int ANGLE_MOTOR = 16;
+        public static final int ENCODER = 7;
     }
 
     public class HopperPorts {
--- src/main/java/frc/robot/subsystems/AutoShooter.java
+++ src/main/java/frc/robot/subsystems/AutoShooter.java
@@ -5,6 +5,8 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -13,7 +15,8 @@ public class AutoShooter extends SubsystemBase {
     private final TalonFX shooterMotor; // motor for starting the rollers
     private final TalonFX angleMotor; // motor for adjusting shooter to desired angle
     private final DutyCycleEncoder encoder;
-    
+    private final PIDController shooterPID;
+    private final SimpleMotorFeedforward shooterFF;
 
 
     public static void configureTalonMotor(TalonFX motor, double currentLimit, NeutralModeValue mode) {
